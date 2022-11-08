f9990c5b526e07845afbd5104a6f8bfdf28341b6
Wojciech Bednarczyk <wojciechbednarczyk25@gmail.com>
2022-11-08 23:57:18 +0100

    Implemented authorization - frontend

--- core/akademia-treningu/src/main/java/pl/edu/pwr/akademiatreningu/security/controller/AuthController.java
+++ core/akademia-treningu/src/main/java/pl/edu/pwr/akademiatreningu/security/controller/AuthController.java
@@ -25,7 +25,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -1,28 +1,35 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {MatButtonModule} from '@angular/material/button';
 import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MAT_DATE_LOCALE, MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import {PasswordResetComponent} from './password-reset/password-reset.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
+import {httpInterceptorProviders} from './helpers/auth.interceptor';
+import {RegisterComponent} from './register/register.component';
+import {DateService} from './services/date.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
+    PasswordResetComponent,
     RegisterComponent,
-    PasswordResetComponent
   ],
   imports: [
     BrowserModule,
@@ -36,8 +43,16 @@ import {PasswordResetComponent} from './password-reset/password-reset.component'
     MatNativeDateModule,
     MatRippleModule,
     MatSelectModule,
+    FormsModule,
+    HttpClientModule,
+    MatMenuModule,
+    MatIconModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+    httpInterceptorProviders,
+    DateService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
